f9bcd4f447200aae8ea751720516783e173f4567
smu7777 <kimhj5727@gmail.com>
2025-04-10 17:36:16 +0900

    hiworld

--- gugudan.py
+++ gugudan.py
@@ -1,11 +1,11 @@
 a,b,g=0,0,""
 
-for a in range(2, 10):
-    g=g+("#   %d단   #" % a)
+for a in range(2,10):
+    g=g+("#  %d단   #"%a)
 print(g)
 
-for a in range(1, 10):
+for a in range(1,10):
     g=""
-    for b in range(2, 10):
-        g=g+str("%2dX %2d= %2d " % (b,a,b*a))
+    for b in range(2,10):
+        g=g+str("%2dX %2d= %2d"%(b,a,b*a))
     print(g)
